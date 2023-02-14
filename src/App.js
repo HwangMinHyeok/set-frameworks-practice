f929d22c9995b6cf09e1819fb5c5212e79aa7511
HwangMinHyeok <aldur930@gmail.com>
2023-02-14 23:44:01 +0900

    copying page without implementing functionalities

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const BtnWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const CircleBtn = styled.button`
+  width: 50px;
+  height: 50px;
+  margin: 5px;
+  font-size: 25px;
+  border: none;
+  border-radius: 100%;
+  background-color: #61dafb;
+`;
 
 function App() {
+  const numFromReduxStore = 3;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {numFromReduxStore}
+        <BtnWrapper>
+          <CircleBtn>+</CircleBtn>
+          <CircleBtn>-</CircleBtn>
+        </BtnWrapper>
       </header>
     </div>
   );
